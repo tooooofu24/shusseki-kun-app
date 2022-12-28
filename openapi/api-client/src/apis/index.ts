e1c21f8258632757aec4a854092de3e9e358be12
tooooofu24 <toya24xxx@i.softbank.jp>
2022-12-29 03:05:07 +0900

    formatterの修正

--- openapi/api-client/src/apis/index.ts
+++ openapi/api-client/src/apis/index.ts
@@ -1,12 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from "./AttendanceApi";
-export * from "./ClassroomApi";
-export * from "./ClientIPApi";
-export * from "./CourseApi";
-export * from "./CourseLogApi";
-export * from "./ScheduleApi";
-export * from "./StudentApi";
-export * from "./SubjectApi";
-export * from "./UserApi";
-export * from "./UserInvitationApi";
+export * from './AttendanceApi';
+export * from './ClassroomApi';
+export * from './ClientIPApi';
+export * from './CourseApi';
+export * from './CourseLogApi';
+export * from './ScheduleApi';
+export * from './StudentApi';
+export * from './SubjectApi';
+export * from './UserApi';
+export * from './UserInvitationApi';
